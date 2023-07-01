3e0b71d1bf1b8b084ee7bfe86f7790b882e5f713
King <kingbian72@gmail.com>
2023-07-01 14:20:08 -0400

    modified matrixMultiplication function

--- matrix.c
+++ matrix.c
@@ -1,13 +1,7 @@
-// #include <SDL2/SDL.h>
-#include <math.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-#define ROWS 3
-#define COLS 3
+#include "matrix.h"
 
 void printMatrix(int matrix[ROWS][COLS]) {
-  printf("\nresult\n{");
+  printf("\nRotational matrix\n{");
   for (int i = 0; i < ROWS; i++) {
     for (int j = 0; j < COLS; j++) {
       printf("%d ", matrix[i][j]);
@@ -18,31 +12,18 @@ void printMatrix(int matrix[ROWS][COLS]) {
 }
 
 
-void matrixMultiplication(int x[ROWS][COLS], int y[ROWS][COLS], int z[ROWS][COLS]) {
-  int r[ROWS][COLS];
+void matrixMultiplication(int firstMatrix[][COLS], int secondMatrix[][COLS],
+                          int resultMatrix[][COLS]) {
 
-  int temp[ROWS][COLS];
   for (int i = 0; i < ROWS; i++) {
     for (int j = 0; j < COLS; j++) {
       for (int k = 0; k < COLS; k++) {
-        temp[i][j] += x[i][k] * y[k][j];
+        resultMatrix[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
       }
     }
   }
 
-  // multiple z and temp
-  for (int i = 0; i < ROWS; i++) {
-    for (int j = 0; j < COLS; j++) {
-      for (int k = 0; k < COLS; k++) {
-        r[i][j] += z[i][k] * temp[k][j];
-      }
-    }
-  }
-
-  printMatrix(r);
 }
-
-
 
 void matrices(int a, int b, int c) {
   int x[ROWS][COLS] = {{1, 0, 0}, {0, cos(a), -sin(a)}, {0, sin(a), cos(a)}};
@@ -51,16 +32,12 @@ void matrices(int a, int b, int c) {
 
   int z[ROWS][COLS] = {{cos(c), -sin(c), 0}, {sin(c), cos(c), 0}, {0, 0, 1}};
 
-  matrixMultiplication(x, y, z);
+// store the result
+  int result[ROWS][COLS]; 
+  
+  matrixMultiplication(x, y, result);
+  matrixMultiplication(result, z, result);
+
+  printMatrix(result);
 }
 
-// int** allocateMatrix(int rows, int cols){
-
-//   int **matrix= malloc(sizeof(int *)* rows);
-  
-//   for(int i=0; i<rows; i++){
-//     matrix[i]= malloc(sizeof(int *)* cols);
-//   }
-
-//   return matrix;
-// }
--- matrix.h
+++ matrix.h
@@ -1,10 +1,15 @@
 #ifndef MATRIX_H
 #define MATRIX_H
 
+// #include <SDL2/SDL.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 #define ROWS 3
 #define COLS 3
 
-void matrixMultiplication(int x[ROWS][COLS], int y[ROWS][COLS], int z[ROWS][COLS]);
+void matrixMultiplication(int firstMatrix[][COLS], int secondMatrix[][COLS], int resultMatrix[][COLS]);
 void matrices(int a, int b, int c);
 void printMatrix(int matrix[ROWS][COLS]);
 

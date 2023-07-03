2fe2c5335565efc8aad5615d6b0d85bd86b731da
King <kingbian72@gmail.com>
2023-07-03 19:33:06 -0400

    implemented vulkan instance  and surface initializatio, added helper method to get available physical devices from the system

--- render_screen.h
+++ render_screen.h
@@ -4,6 +4,8 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <SDL2/SDL_vulkan.h>
+#include <vulkan/vulkan.h>
 
 int createScreen();
 void drawPoints(SDL_Renderer *renderer, SDL_Window *screen, int pointX,
